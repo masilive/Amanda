ed8f7daccfe25cb90211e37b7867e8ea663bde67
Masilive Sifanele <masilive.sifanele@gmail.com>
2023-04-14 19:19:01 +0200

    added chart with notifications

---
+++ Chart/js/index.js
@@ -0,0 +1,31 @@
+﻿const handleButtonPrevClicked = () => {
+    window.external.notify('Button Prev Clicked');
+}
+const handleButtonPlayClicked = () => {
+    window.external.notify('Button Play Clicked');
+}
+const handleButtonNextClicked = () => {
+    window.external.notify('Button Next Clicked');
+}
+
+window.showNotification = (message, type = 'info', timeLimit = 3000) => {
+    const alertContainer = document.getElementById('alertContainer');
+    const elementId = alertContainer.childElementCount + 1;
+
+    const onCloseButtonClicked = (elementSelector) => {
+        $(elementSelector).remove();
+        window.external.notify(elementSelector);
+    }
+
+    const divAlert = `<div id="notif-${elementId}" class="alert alert-${type.toLowerCase()} alert-dismissible fade show m-1"><svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#${type}-fill" /></svg> ${message}.<button type="button" class="btn-close" data-bs-dismiss="alert" onClick="onCloseButtonClicked('#notif-${elementId}')"></button></div>`;
+
+    alertContainer.insertAdjacentHTML('beforeend', divAlert);
+
+    setTimeout(function () {
+        $(`#notif-${elementId}`).fadeTo(2000, .1);
+
+        setTimeout(function () {
+            onCloseButtonClicked(`#notif-${elementId}`);
+        }, 2000);
+    }, timeLimit);
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Amanda.Controllers;
+using Amanda.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +26,37 @@ namespace Amanda
     /// </summary>
     public sealed partial class MainPage : Page
     {
+		public delegate void ShowNotificationHandler(string message, NotificationTypeEnum notificationType);
+		public static event ShowNotificationHandler ShowNotification;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
-    }
+
+		private void Page_Loaded(object sender, RoutedEventArgs e)
+		{
+			ShowNotification += MainPageShowNotification;
+		}
+
+		private void MainPageShowNotification(string message, NotificationTypeEnum notificationType)
+		{
+			_ = AddNotification(message, notificationType);
+		}
+
+		private async Task AddNotification(string message, NotificationTypeEnum notificationType)
+		{
+			_ = await WebViewMain.InvokeScriptAsync("showNotification", new string[] { message, notificationType.ToString() });
+		}
+
+		private void WebViewMain_ScriptNotify(object sender, NotifyEventArgs e)
+		{
+			
+		}
+
+		private void WebViewMain_Loaded(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
 }
